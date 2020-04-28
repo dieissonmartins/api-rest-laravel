1e3856d49ca838786b20e33f99603df23ab9bfbb
dieissonmartins <dieisson.martins.santos@gmail.com>
2020-04-28 11:34:29 -0300

    add campos na tabela real_state

--- database/migrations/2020_04_27_161936_create_table_real_state.php
+++ database/migrations/2020_04_27_161936_create_table_real_state.php
@@ -14,7 +14,18 @@ class CreateTableRealState extends Migration
     public function up()
     {
         Schema::create('real_state', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
+            $table->float('price', 10, 2);
+            $table->integer('bathrooms');
+            $table->integer('badrooms');
+            $table->integer('property_area');
+            $table->integer('total_property_area');
+            $table->string('slug');
+           
             $table->timestamps();
         });
     }
